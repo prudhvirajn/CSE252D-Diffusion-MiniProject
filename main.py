566af68cc4c4b26a2ae785f385751eba9a19f2b1
Prudhviraj Naidu <prnaidu@ucsd.edu>
2023-06-10 16:50:15 -0700

    Adding argument parser for input and output filepaths

--- main.py
+++ main.py
@@ -4,18 +4,37 @@ import json
 from model import ImageGen
 from utils import image_grid, save_gif, downsample_img
 
-def main(script_num, gif_duration):
-    dataset_path = "dataset.json"
-    output_filepath = "output.jpg"
-    output_gif_filepath = "output.gif"
+def main(args):
+    dataset_path = args.dataset_path 
+    output_filepath = args.output_filepath
+    output_gif_filepath = args.output_gif_filepath
+    script_num = args.script_num
+    gif_duration = args.gif_duration
+    interp_steps = args.interp_steps
+    prefix_id = args.prefix_num
+    style_id = args.style_id
     
     with open(dataset_path, "r") as file:
         dataset = json.load(file)
+
+
+    if script_num < 0 or script_num > len(dataset["scripts"]):
+        print(f"Invalid script number. (Choose between [0,{len(dataset['scripts'])}]. Exiting..)")
+        exit()
+    elif prefix_id < 0 or prefix_id > len(dataset["prefixes"]):
+        print(f"Invalid prefix number. (Choose between [0,{len(dataset['prefixes'])}]. Exiting..)")
+        exit()
+    elif style_id < 0 or style_id > len(dataset["suffixes"]):
+        print(f"Invalid suffix number. (Choose between [0,{len(dataset['suffixes'])}]. Exiting..)")
+        exit()
+    elif interp_steps < 0:
+        print("Invalid interpolation steps ([0,inf]). Exiting..")
+        exit()
         
     negative_prompt = dataset["negative_prompt"]
     
-    prefix = dataset["prefixes"][0]
-    suffix = dataset["suffixes"][0]
+    prefix = dataset["prefixes"][prefix_id]
+    suffix = dataset["suffixes"][style_id]
     
     script = dataset["scripts"][script_num]
     
@@ -49,7 +68,11 @@ if __name__ == '__main__':
     parser.add_argument("--duration", "-d", dest="gif_duration", type=int, action="store", default=300, required=False, help="Duration (ms) for each frame in gif")
     parser.add_argument("--interpolation_steps", "-i", dest="interp_steps", type=int, action="store", default=0, required=False, help="Number of interpolation steps between story keypoints")
     parser.add_argument("--prefix_num", "-p", dest="prefix_num", type=int, action="store", default=0, required=False, help=f"Choose a prefix number for the text prompt [0,{MAX_PREFIX}]")
-    parser.add_argument("--suffix_num", "-suf", dest="suffix_num", type=int, action="store", default=0, required=False, help="Choose a suffix number for the text prompt [0,{MAX_SUFFIX}]")
+    parser.add_argument("--suffix_num", "-suf", dest="style_id", type=int, action="store", default=0, required=False, help="Choose a suffix number for the text prompt [0,{MAX_SUFFIX}]")
+    parser.add_argument("--dataset_path", "-dp", dest="dataset_path", type=str, action="store", default="dataset.json", required=False, help="Path to dataset json file")
+    parser.add_argument("--output_filepath", "-o", dest="output_filepath", type=str, action="store", default="output.jpg", required=False, help="Path to output image file")
+    parser.add_argument("--output_gif_filepath", "-og", dest="output_gif_filepath", type=str, action="store", default="output.gif", required=False, help="Path to output gif file")
+
     args = parser.parse_args()
 
     if args.script_num < 0 or args.script_num > MAX_SCRIPT:
@@ -59,4 +82,4 @@ if __name__ == '__main__':
       print("Invalid gif duration ([0,inf]). Exiting..")
       exit()
 
-    main(args.script_num, args.gif_duration)
+    main(args)
